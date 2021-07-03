a1671c97e426eba6cb803363707d1b16ab31c002
j92pruitt <j92pruitt@gmail.com>
2021-07-03 08:33:49 -0700

    Created basic logic for getting input from user.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Questions to gather information for building readme.
+const questions = [
+    {
+        type: "input",
+        message: "What is the title?",
+        name: "title"
+    }
+];
 
 // Function call to initialize app
 init();
+
+function init() {
+    inquirer.prompt(questions)
+        .then(
+            ( {title} ) => console.log(title)
+        )
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
